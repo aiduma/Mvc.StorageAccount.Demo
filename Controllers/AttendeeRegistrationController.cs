7ab6c6cf6663a8960e411ace69bcbb5cd21b95ff
aiduma <aiduma08@gmail.com>
2025-02-24 20:35:23 -0700

    Added feature to save data storage

---
+++ Controllers/AttendeeRegistrationController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mvc.StorageAccount.Demo.Data;
+using Mvc.StorageAccount.Demo.Services;
+
+namespace Mvc.StorageAccount.Demo.Controllers
+{
+    public class AttendeeRegistrationController : Controller
+    {
+        private readonly ITableStorageService _tableStorageService;
+
+        public AttendeeRegistrationController(ITableStorageService tableStorageService )
+        {
+            _tableStorageService = tableStorageService;
+        }
+
+        // GET: AttendeeRegistratioController
+        public async Task<ActionResult> Index()
+        {
+            var data = await _tableStorageService.GetAttendees();
+            return View(data);
+        }
+
+        // GET: AttendeeRegistratioController/Details/5
+        public async Task<ActionResult> Details(string id, string industry)
+        {
+            var data = await _tableStorageService.GetAttendee(industry, id);
+            return View(data);
+        }
+
+        // GET: AttendeeRegistratioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AttendeeRegistratioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(AttendeeEntity attendeeEntity)
+        {
+            try
+            {
+                attendeeEntity.PartitionKey = attendeeEntity.Industry;
+                attendeeEntity.RowKey = Guid.NewGuid().ToString();
+
+                await _tableStorageService.UpsertAttendee(attendeeEntity);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AttendeeRegistratioController/Edit/5
+        public async Task<ActionResult> Edit(string id, string industry)
+        {
+            var data = await _tableStorageService.GetAttendee(industry, id);
+            return View(data);
+        }
+
+        // POST: AttendeeRegistratioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, AttendeeEntity attendeeEntity)
+        {
+            try
+            {
+                attendeeEntity.PartitionKey = attendeeEntity.Industry;
+                await _tableStorageService.UpsertAttendee(attendeeEntity);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AttendeeRegistratioController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View(data);
+        //}
+
+
+        // POST: AttendeeRegistratioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(string id, string industry)
+        {
+            try
+            {
+                await _tableStorageService.DeleteAttendee(industry, id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
